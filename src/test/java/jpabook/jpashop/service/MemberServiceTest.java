a22d4d8fab631421076e0ab6348d4ec09fbcad38
geombong <shoy1415@gmail.com>
2022-05-18 17:15:44 +0900

    테스트

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,60 @@
+package jpabook.jpashop.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("회원가입")
+    void joinTest() {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            assertThat(memberRepository.findOne(savedId)).as("회원 가입 테스트").isEqualTo(member);
+        });
+    }
+
+    @Test
+    @DisplayName("중복회원예외")
+    void duplicateTest() {
+        //given
+        Member memberA = new Member();
+        memberA.setName("kim");
+
+        Member memberB = new Member();
+        memberB.setName("kim");
+
+        //when
+        memberService.join(memberA);
+
+        //then
+        assertThatThrownBy(() -> {
+            memberService.join(memberB);
+        }).isInstanceOf(IllegalStateException.class)
+                .hasMessage("이미 존재하는 회원입니다.");
+    }
+}
