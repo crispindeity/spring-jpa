7732151c1085ed265370446d671076b3b643a48c
geombong <shoy1415@gmail.com>
2022-05-19 23:01:59 +0900

    Order

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.repository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import jpabook.jpashop.domain.Order;
+
+@Repository
+public class OrderRepository {
+
+    private final EntityManager entityManager;
+
+    public OrderRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public void save(Order order) {
+        entityManager.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return entityManager.find(Order.class, id);
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//
+//    }
+}
